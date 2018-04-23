f6fd965761cd4f3a43e8630d970b10adb797307a
IsmaelKhalil <ismael.khalil1995@gmail.com>
2018-04-23 12:02:23 -0400

    Configured swagger

--- src/main/java/com/revature/gambit/gateway/Application.java
+++ src/main/java/com/revature/gambit/gateway/Application.java
@@ -2,15 +2,59 @@ package com.revature.gambit.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/***
+ * Application
+ * 
+ * Runs the Zuul API Gateway.
+ * 
+ * @author Ismael Khalil
+ */
 
 @SpringBootApplication
 @EnableZuulProxy
-@RestController
+@EnableDiscoveryClient
+@CrossOrigin
+@EnableSwagger2
 public class Application {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	/***
+	 * CORSFilter
+	 * 
+	 * Sets up CORSFilter for Zuul.
+	 * 
+	 * @return new CorsFilter(source)
+	 */
+	
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration("/**", config);
+
+		return new CorsFilter(source);
 	}
 }
---
+++ src/main/java/com/revature/gambit/gateway/swagger/DocumentationController.java
@@ -0,0 +1,45 @@
+package com.revature.gambit.gateway.swagger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+/***
+ * DocumentationController is responsible for establishing Swagger
+ * documentation. Swagger ui can be accessed at:
+ * http://localhost:8800/swagger-ui.html
+ * 
+ * @author Unknown
+ */
+
+@Component
+@Primary
+@EnableAutoConfiguration
+public class DocumentationController implements SwaggerResourcesProvider {
+
+	@Override
+	public List<SwaggerResource> get() {
+		List<SwaggerResource> resources = new ArrayList<>();
+		resources.add(swaggerResource("batches", "/api/v2/users/v2/api-docs", "2.0"));
+		resources.add(swaggerResource("trainers", "/api/v2/users/v2/api-docs", "2.0"));
+		resources.add(swaggerResource("trainees", "/api/v2/users/v2/api-docs", "2.0"));
+		resources.add(swaggerResource("users", "/api/v2/users/v2/api-docs", "2.0"));
+
+		return resources;
+	}
+
+	private SwaggerResource swaggerResource(String name, String location, String version) {
+		SwaggerResource swaggerResource = new SwaggerResource();
+		swaggerResource.setName(name);
+		swaggerResource.setLocation(location);
+		swaggerResource.setSwaggerVersion(version);
+
+		return swaggerResource;
+	}
+}
