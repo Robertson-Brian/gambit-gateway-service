a5a84f1c89b8eea55641ca96604e0e03d4d8aa67
Peter Farber <peterfarber@fullsail.edu>
2018-04-24 11:24:26 -0400

    Working with Config Server!

--- src/main/java/com/revature/hydra/gateway/Application.java
+++ src/main/java/com/revature/hydra/gateway/Application.java
@@ -2,12 +2,13 @@ package com.revature.hydra.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableZuulProxy
-@RestController
+@EnableEurekaClient
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
